144ea619daa3b4da3023d0a3164e906512e20720
Enguang Song <esong54@outlook.com>
2024-08-18 01:14:01 -0400

    add stream and user auth router

---
+++ routes/streamRoutes.mjs
@@ -0,0 +1,71 @@
+// routes/streamRoutes.js
+import Router from 'koa-router';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { getLibraryIndex } from '../models/library.mjs';
+import { library, user } from '../models/dbConnections.mjs'; 
+
+const router = new Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const libraryIndex = getLibraryIndex(library);
+
+// 获取所有音轨
+router.get('/api/stream', async (ctx) => {
+  try {
+    const tracks = await libraryIndex.find(); 
+    ctx.body = { tracks };
+  } catch (err) {
+    console.log(`Error retrieving tracks: ${err}`);
+    ctx.status = 500;
+    ctx.body = 'Internal Server Error';
+  }
+});
+
+// 通过 `track_id` 串流音频文件
+router.get('/api/stream/:track_id', async ctx => {
+  try {
+    const trackId = ctx.params.track_id;
+    const track = await libraryIndex.findOne({ track_id: trackId });
+
+    if (!track) {
+      console.log("Track not found!");
+      ctx.status = 404;
+      ctx.body = { error: 'Track not found' };
+      return;
+    }
+
+    const filePath = path.join(__dirname, '../public', track.file.replace(/\\/g, '/'));
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = ctx.headers.range;
+
+    console.log(`Playing: ${filePath}`);
+
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(filePath, { start, end });
+        ctx.set('Content-Range', `bytes ${start}-${end}/${fileSize}`);
+        ctx.set('Accept-Ranges', 'bytes');
+        ctx.set('Content-Length', chunksize);
+        ctx.set('Content-Type', 'audio/mpeg');
+        ctx.status = 206;
+        ctx.body = file;
+    } else {
+        ctx.set('Content-Length', fileSize);
+        ctx.set('Content-Type', 'audio/mpeg');
+        ctx.body = fs.createReadStream(filePath);
+    }
+  } catch (err) {
+    console.log(`${err}`);
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to stream track' };
+  }
+});
+
+export default router;
---
+++ routes/userRouter.mjs
@@ -0,0 +1,103 @@
+// routes/userRouter.mjs
+import Router from 'koa-router';
+import { getUserIndex } from '../models/users.mjs';
+import {  user } from '../models/dbConnections.mjs'; 
+
+const router = new Router();
+const userIndex = getUserIndex(user);
+
+
+// handle login request
+router.post('/auth/login', async (ctx) => {
+    const { uid, secret } = ctx.request.body;
+    console.log(`${uid}: ${secret}`);
+
+    try {
+        // find user
+        const user = await userIndex.findOne({ uid, secret });
+
+        console.log(`find user: ${user}`);
+        if (user) {
+            // ctx.body = {
+            //     status: 0,
+            //     msg: "Success"
+            // };
+            ctx.body = {
+                status: 0,
+                msg: "Login successful",
+                user: {
+                    uid: user.uid,
+                    name: user.name,
+                    subscribe: user.subscribe,
+                    subscribe_expired: user.subscribe_expired,
+                    last_login: user.last_login,
+                    playing: user.playing
+                }
+            };
+        } else {
+            ctx.body = {
+                status: 1,
+                msg: "Username or Password error."
+            };
+        }
+    } catch (err) {
+        console.log(`err: ${err}`);
+        ctx.status = 500;
+        ctx.body = {
+            status: 1,
+            msg: "Internal Server Error"
+        };
+    }
+});
+
+// handle register request
+router.post('/auth/register', async (ctx) => {
+    const { uid, name, secret } = ctx.request.body;
+    console.log(`Registration attempt: ${uid}`);
+
+
+    try {
+        // check if user alread exist
+        const existingUser = await userIndex.findOne({ uid });
+        if (existingUser) {
+            ctx.body = {
+                status: 1,
+                msg: "User Already Exist."
+            };
+            return;
+        }
+
+        const subscribeDuration = {
+            'Free': null,
+            'Monthly': new Date(new Date().setMonth(new Date().getMonth() + 1)), // add one month
+            'Yearly': new Date(new Date().setFullYear(new Date().getFullYear() + 1)) // add a year
+        };
+
+        // new user
+        const newUser = new userIndex({
+            uid,
+            name,
+            secret,
+            subscribe: 'Free', // default
+            subscribe_expired: null,
+            last_login: new Date(),
+            playing: ''
+        });
+
+        console.log(`new user register: ${newUser}`)
+
+        await newUser.save();
+            ctx.body = {
+                status: 0,
+                msg: "Registration successful"
+            };
+    } catch (err) {
+        ctx.status = 500;
+        ctx.body = {
+            status: 1,
+            msg: "Internal Server Error"
+        };
+    }
+});
+
+export default router;
